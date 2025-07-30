ecbc028ea03266a4a2b28bb2aba0dd3c9526bd3d
Pirasanthan Jesugeevegan <pirasanth23@hotmail.com>
2025-07-30 14:55:29 +0100

    feat: enhance frontend with Tailwind CSS, Radix UI components, and theme management

---
+++ apps/frontend/tailwind.config.js
@@ -0,0 +1,38 @@
+import colors from 'tailwindcss/colors';
+
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [],
+};
